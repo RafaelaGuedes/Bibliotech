725a240174d14fde9cd5e663dbfd72c791ca8201
RafaelaGuedes <rafaela.guedes.santos@gmail.com>
2018-05-21 20:07:28 -0300

    Criação dos CRUDs de Autor e Editora

---
+++ Bibliotech/Models/Editora.cs
@@ -0,0 +1,29 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+
+namespace Bibliotech.Models
+{
+    [Serializable]
+    public class Editora
+{
+    public virtual Guid? Id { get; set; }
+
+    public virtual string Nome { get; set; }
+
+    public virtual byte[] Version { get; set; }
+    }
+public class EditoraMap : ClassMap<Editora>
+{
+    public EditoraMap()
+    {
+        Id(x => x.Id).GeneratedBy.GuidNative();
+        Map(x => x.Nome).Length(200).Not.Nullable();
+
+         Version(x => x.Version)
+                .Nullable()
+                .CustomSqlType("timestamp")
+                .Generated.Always();
+    }
+
+}
+}
