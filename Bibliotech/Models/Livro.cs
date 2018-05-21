e465ad2a1bbb0d011b60d3cf758a029e81633378
André Silveira <a.eliaas@gmail.com>
2018-05-21 20:27:59 -0300

    Criação do CRUD de Livro e Exemplar

--- Bibliotech/Models/Livro.cs
+++ Bibliotech/Models/Livro.cs
@@ -1,8 +1,10 @@
 ﻿using FluentNHibernate.Mapping;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace Bibliotech.Models
 {
@@ -11,14 +13,19 @@ namespace Bibliotech.Models
     {
         public virtual Guid? Id { get; set; }
 
+        [Display(Name = "Título")]
         public virtual string Titulo { get; set; }
 
+        [Display(Name = "ISBN")]
         public virtual string Isbn { get; set; }
 
+        [Display(Name = "Ano de Publicação")]
         public virtual int? AnoPublicao { get; set; }
 
+        [Display(Name = "Edição")]
         public virtual int? Edicao { get; set; }
 
+        [Display(Name = "Número de Páginas")]
         public virtual int? NumeroPaginas { get; set; }
 
         public virtual string Assunto { get; set; }
@@ -26,6 +33,9 @@ namespace Bibliotech.Models
         public virtual string NomeArquivo { get; set; }
 
         public virtual byte[] Version { get; set; }
+
+        [XmlIgnore]
+        public virtual IList<Exemplar> Exemplares { get; set; }
     }
 
     public class LivroMap : ClassMap<Livro>
@@ -41,6 +51,8 @@ namespace Bibliotech.Models
             Map(x => x.NumeroPaginas);
             Map(x => x.Assunto).Length(200);
             Map(x => x.NomeArquivo).Length(300);
+
+            HasMany<Exemplar>(x => x.Exemplares).Cascade.All().Inverse().LazyLoad();
 
             Version(x => x.Version)
                 .Nullable()
